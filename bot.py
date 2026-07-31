558acfa4bd68484681b2f9c880cb5329fa435719
Rajsingh1432 <152856503+Rajsingh1432@users.noreply.github.com>
2026-08-01 00:29:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -143,7 +143,7 @@ user_msg_counter = {}
 
 # ⭐ Personal DM me /start chhorke kuchh bhi msg aane par random reply (bina API)
 DM_ONLY_REPLIES = [
-    "🌿 Mai sirf chating groups me baat karti hun\n🌿 Personal Dm Mat kro\n🌿 Mujhse Baat Karna Hai To Mujhe Apne Chating Group Me Add Kardo",
+    "🌿 Mai sirf chating groups me baat karti hun\n🌿 Personal Dm Mat kro\n🌿 Mujhse Baat Karna Hai To Mujhe Apne Chating Group Me Add Kardo\n🌿 Aur han mai sab new members ka @username mention karke wecome karung\n\n\n 𝐍𝐨𝐭𝐞 𒊹︎︎︎𝐀𝐝𝐦𝐢𝐧 𝐃𝐞𝐧𝐚 𝐙𝐚𝐫𝐮𝐫𝐢 𝐇𝐚𝐢 𝐅𝐫𝐞𝐞 𝐌𝐞 𝐌𝐞𝐡𝐧𝐚𝐭 𝐍𝐡𝐢 𝐊𝐚𝐫𝐭𝐢 𝐌𝐚𝐢 𝐀𝐛 𝐉𝐚𝐥𝐝𝐢 𝐒𝐞 𝐍𝐞𝐞𝐜𝐡𝐞 𝐁𝐮𝐭𝐭𝐨𝐧 𝐃𝐚𝐛𝐚𝐨 𝐀𝐮𝐫 𝐀𝐝𝐝 𝐊𝐚𝐫𝐥𝐨 𝐌𝐮𝐣𝐡𝐞 👇",
 ]
 
 # ⭐ track kiya hua users jinko already welcome mil chuka (duplicate welcome rokne ke liye)
